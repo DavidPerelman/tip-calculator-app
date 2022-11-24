d0d1178d9aa07309b3e30ae2d2b4358ae0f0be1d
David Perelman <dperelman3@gmail.com>
2022-11-25 01:07:15 +0200

    setup the bill input value should be 0 test

--- src/containers/CalculatorInputs/CalculatorInputs.js
+++ src/containers/CalculatorInputs/CalculatorInputs.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '../../components/Container/Container';
-import bill from '../../asset/bill.png';
+import header from '../../asset/bill.png';
 import './CalculatorInputs.css';
 
 const CalculatorInputs = () => {
+  const [bill, setBill] = useState(0);
+
   return (
     <div data-testid='CalculatorInputs'>
       <Container size='medium' color='white'>
         <div id='bill-input'>
-          <img src={bill} className='bill' alt='bill' />
-          <input className='bill-input' value='0' />
+          <img src={header} className='bill-header' alt='bill-header' />
+          <input
+            type='text'
+            data-testid='billInput'
+            className='bill-input'
+            value={bill}
+          />
         </div>
       </Container>
     </div>
--- src/containers/CalculatorInputs/__test__/CalculatorInputs.test.js
+++ src/containers/CalculatorInputs/__test__/CalculatorInputs.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from '../../../App';
 
 describe('CalculatorInputs component', () => {
-  test('test if the conatainer is in the page and have corect className', async () => {
+  test('test if the conatainer is in the page and have corect className', () => {
     render(<App />);
 
     const calculatorInputsContainer = screen.getByTestId('CalculatorInputs');
@@ -11,5 +11,13 @@ describe('CalculatorInputs component', () => {
     expect(calculatorInputsContainer.firstChild).toHaveClass(
       'Container medium white'
     );
+  });
+
+  test('the bill input value should be 0', () => {
+    render(<App />);
+
+    const billInput = screen.getByTestId('billInput');
+
+    expect(billInput.value).toBe('0');
   });
 });
