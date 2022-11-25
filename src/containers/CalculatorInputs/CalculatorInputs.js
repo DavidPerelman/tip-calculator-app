5cbc2aa8c171dc807c5eec4ad2b2afc7e89b932c
David Perelman <dperelman3@gmail.com>
2022-11-25 02:20:46 +0200

    setup numberOfPeople

--- src/containers/CalculatorInputs/CalculatorInputs.js
+++ src/containers/CalculatorInputs/CalculatorInputs.js
@@ -1,26 +1,44 @@
 import React, { useState } from 'react';
 import Container from '../../components/Container/Container';
-import header from '../../asset/bill.png';
+import billHeader from '../../asset/bill.png';
+import numberOfPeopleIconHeader from '../../asset/number-of-people.png';
 import './CalculatorInputs.css';
 
 const CalculatorInputs = () => {
   const [bill, setBill] = useState('');
-
-  console.log(bill);
+  const [numberOfPeople, setNumberOfPeople] = useState('');
 
   return (
     <div data-testid='CalculatorInputs'>
       <Container size='medium' color='white'>
         <div id='bill-input'>
-          <img src={header} className='bill-header' alt='bill-header' />
+          <img src={billHeader} className='bill-header' alt='bill-header' />
           <div class='wrapper'>
-            <div class='icon'></div>
+            <div class='dollar-icon'></div>
             <input
               type='text'
               onChange={(e) => setBill(e.target.value)}
               data-testid='billInput'
               className='bill-input'
               value={bill}
+            />
+          </div>
+        </div>
+
+        <div id='bill-input'>
+          <img
+            src={numberOfPeopleIconHeader}
+            className='number-of-people'
+            alt='number-of-people'
+          />
+          <div class='wrapper'>
+            <div class='person-icon'></div>
+            <input
+              type='text'
+              onChange={(e) => setNumberOfPeople(e.target.value)}
+              data-testid='billInput'
+              className='bill-input'
+              value={numberOfPeople}
             />
           </div>
         </div>
